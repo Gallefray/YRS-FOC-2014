a88282659cdf0ad893602e282b30c7f7a07179fb
finnoleary <finnoleary@inventati.org>
2014-07-30 14:11:09 +0100

    working-p

--- test.js
+++ test.js
@@ -2,10 +2,11 @@
 function foo() {
 	var data = "";
 	var dest = "./data/gov-spend-2012.json"; //"./data/test.json"; //
-	console.log("boop");
-	$.get(dest, function(d) {console.log(d);});
+	$.getJSON(dest, function(d) 
+					{
+						data = d;
+					});
 	console.log(data);
-	console.log("\nBop\n");
 	//$("section > p").append(data);
 }
 
